081160f8f21f6fd02ddcaf8e6475a2dfba1a77d2
Alex Ells <8273272+alexells@users.noreply.github.com>
2020-12-02 18:01:16 -0500

    improve data reading

--- day2/part2.py
+++ day2/part2.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 
 # load data
-df = pd.read_csv("day2/input.txt", delimiter="-| |: ", header=None)
-df.columns = ["pos1", "pos2", "letter", "pw"]
+df = pd.read_csv(
+    "day2/input.txt",
+    delimiter="-| |: ",
+    header=None,
+    names=["pos1", "pos2", "letter", "pw"],
+)
 
-# check occurances of required letters
+# check occurences of required letters
 df["check_p1"] = df.apply(lambda x: x.pw[x.pos1 - 1] == x.letter, axis=1)
 df["check_p2"] = df.apply(lambda x: x.pw[x.pos2 - 1] == x.letter, axis=1)
 df["exactly_one_match"] = np.logical_xor(df["check_p1"], df["check_p2"])
