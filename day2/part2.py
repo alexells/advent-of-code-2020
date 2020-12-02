d4a945c3847afdaf26952faf364059532beb399d
Alex Ells <8273272+alexells@users.noreply.github.com>
2020-12-02 17:57:56 -0500

    improve delimiter to avoid unnecessary columns

--- day2/part2.py
+++ day2/part2.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 
 # load data
-df = pd.read_csv("day2/input.txt", delimiter="\s+|;|:|-", header=None)
-df.columns = ["pos1", "pos2", "letter", "whitespace", "pw"]
-df = df.drop(columns="whitespace")
+df = pd.read_csv("day2/input.txt", delimiter="-| |: ", header=None)
+df.columns = ["pos1", "pos2", "letter", "pw"]
 
 # check occurances of required letters
 df["check_p1"] = df.apply(lambda x: x.pw[x.pos1 - 1] == x.letter, axis=1)
